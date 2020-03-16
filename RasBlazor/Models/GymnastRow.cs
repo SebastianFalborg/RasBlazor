0b86da1be5f577ae6194c8d9d184b74b18210164
Sebastian Falborg <sebastianfalborg@gmail.com>
2020-03-17 00:38:14 +0100

    Add Floor and list with styling and functionality

---
+++ RasBlazor/Models/GymnastRow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RasBlazor.Models
+{
+    public class GymnastRow
+    {
+        public int RowNumber { get; set; }
+
+        public IList<Gymnast> Gymnasts { get; set; } = new List<Gymnast>();
+    }
+}
